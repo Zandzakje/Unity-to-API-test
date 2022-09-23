4643c01f130cbc10cc6f9590f0d62f2e7daf8fc7
Zandzakje <DamianTomasila@hotmail.com>
2022-09-23 14:58:16 +0200

    API get operation functional
    - Unity can use the 'get' operation for 1 record and display it on the UI.

--- Assets/Scripts/API_test.cs
+++ Assets/Scripts/API_test.cs
@@ -1,11 +1,18 @@
+using SimpleJSON;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class API_test : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI _itemTitleUI;
+    [SerializeField] private TextMeshProUGUI _itemCompletedUI;
+
     private const string URL = "http://jsonplaceholder.typicode.com/todos";
+    //private const string URL = "http://localhost:59105/ArObjects";
 
     public void GenerateRequest()
     {
@@ -24,7 +31,21 @@ public class API_test : MonoBehaviour
             }
             else
             {
-                Debug.Log(request.downloadHandler.text);
+                // Debug.Log(request.downloadHandler.text);
+
+                JSONNode itemsData = JSON.Parse(request.downloadHandler.text);
+
+
+                int randomNum = Random.Range(1, itemsData.Count);
+
+                Debug.Log("The generated item is: \nTitle: " + itemsData[randomNum]["title"]);
+                //Debug.Log("The generated item is: \nName: " + itemsData[randomNum]["name"]);
+
+                string itemTitle = itemsData[randomNum]["title"];
+                bool itemCompleted = itemsData[randomNum]["completed"];
+
+                _itemTitleUI.text = itemTitle;
+                _itemCompletedUI.text = itemCompleted.ToString();
             }
         }
     }
